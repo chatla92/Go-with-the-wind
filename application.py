ef11a2c143a0db73ea2617af29d6cdce80521896
Anjani Sai Kumar Chatla <anjanichatla92@gmail.com>
2017-05-12 07:07:19 -0700

    Flask settings 3

--- application.py
+++ application.py
@@ -15,7 +15,8 @@ def baseHtml():
 def index(r):
     [start, destination] = r.split('&')
     polyL = getOptimalRoute(start, destination)
-    return jsonify({"points": [start, destination],  "polyline": polyL})
+    return jsonify({"points": [start, destination], "distance": "25", "windText": "Wind will oppose you for 50 % of ride", "climate": "sunny", "polyline": polyL})
 
 if __name__ == '__main__':
     application.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
