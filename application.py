9cc64e97c70d25933d43fecde38e2996a42a2179
Anjani Sai Kumar Chatla <anjanichatla92@gmail.com>
2017-07-06 23:00:07 -0700

    Small changes

--- application.py
+++ application.py
@@ -4,19 +4,19 @@ import sys
 sys.path.append('static')
 from main import getOptimalRoute
 
-application = Flask(__name__,static_folder='static')
+app = Flask(__name__,static_folder='static')
 
 
-@application.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def baseHtml():
-    #return application.send_static_file('index.html')
+    #return app.send_static_file('index.html')
     return render_template('index.html')
-@application.route('/q/<string:r>', methods=['GET'])
+@app.route('/q/<string:r>', methods=['GET'])
 def index(r):
     [start, destination] = r.split('&')
     polyL = getOptimalRoute(start, destination)
     return jsonify({"points": [start, destination], "distance": "25", "windText": "Wind will oppose you for 50 % of ride", "climate": "sunny", "polyline": polyL})
 
 if __name__ == '__main__':
-    application.run(debug=True)
-    #application.run(host='0.0.0.0', port=8000)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=8000)
